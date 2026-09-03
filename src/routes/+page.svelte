496fad6b5cc17caf0b6733ff46ecf820dff7aa2c
Moibe <moi.estrello@gmail.com>
2026-09-03 07:49:57 -0600

    APP_VERSION 0.1.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
   import TurnOrder from '$lib/ui/TurnOrder.svelte';
   import UnitPanel from '$lib/ui/UnitPanel.svelte';
 
-  const APP_VERSION = '0.1.0';
+  const APP_VERSION = '0.1.1';
 
   // ---- Camera -------------------------------------------------------------
 
